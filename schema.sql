1b88356098f354710404d904216956fb1fe252c5
mugisha Samuel <mugishasamuel42@gmail.com>
2022-03-23 01:34:36 -0700

    Queries and updates

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,name VARCHAR(255),date_of_birth DATE,escape_attempts INT,neutered BOOLEAN,weight_kg DECIMAL);
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(255);
